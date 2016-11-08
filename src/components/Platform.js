f8c6d84fbcf06f9f966181368195d00ec2446591
Koty Wong <wongkoty@gmail.com>
2016-11-08 15:10:09 -0500

    adding food descrip

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -12,10 +12,22 @@ export default class Platform extends Component {
         <p className='platform-info text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
-        <div className='food-icon'>
-      		<img src={icon1}/>
-      		<img src={icon2}/>
-      		<img src={icon3}/>
+        <div className='food-icon-container'>
+        	<div className='food-descrip-container'>
+	      		<img src={icon1} className='food-icon'/><br/>
+	      		<b>Who uses the app?</b><br/>
+	      		<p>I'm a really good feature. That's why we want to talk about it</p>
+      		</div>
+      		<div className='food-descrip-container'>
+	      		<img src={icon2} className='food-icon'/><br/>
+	      		<b>Who uses the app?</b><br/>
+	      		<p>I'm a really good feature. That's why we want to talk about it</p>
+      		</div>
+      		<div className='food-descrip-container'>
+	      		<img src={icon3} className='last-food-icon'/><br/>
+	      		<b>Who uses the app?</b><br/>
+	      		<p>I'm a really good feature. That's why we want to talk about it</p>
+      		</div>
       	</div>
       </div>
 
