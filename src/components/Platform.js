eefd59f214b7f1058a7feb1d05a2231243a2bd1e
Koty Wong <wongkoty@gmail.com>
2016-11-08 15:03:13 -0500

    adding icons

--- src/components/Platform.js
+++ src/components/Platform.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import icon1 from '../../public/images/icon_1@2x.png';
+import icon2 from '../../public/images/icon_2@2x.png';
+import icon3 from '../../public/images/icon_3@2x.png';
 
 
 export default class Platform extends Component {
@@ -6,10 +9,16 @@ export default class Platform extends Component {
     return (
       <div className='platform'>
         <h1 className='text-uppercase text-center platform-text'>why use our platform</h1>
-        <p className='text-center platform-info'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+        <p className='platform-info text-center'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi</p>
+        <div className='food-icon'>
+      		<img src={icon1}/>
+      		<img src={icon2}/>
+      		<img src={icon3}/>
+      	</div>
       </div>
+
       )
   }
 
