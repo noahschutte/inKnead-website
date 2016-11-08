8376c74f22a760999fa11382758953039f4c1bbc
Koty Wong <wongkoty@gmail.com>
2016-11-07 21:01:29 -0500

    adding stylesheet, modifying webpack

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import './stylesheets/main.scss';
+require('./stylesheets/main.scss');
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
