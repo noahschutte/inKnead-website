c0b4241f3f33d8fb8074ca28a8513a0e3bfe3a4b
Koty Wong <wongkoty@gmail.com>
2016-11-16 13:49:16 -0500

    deploying

--- server.js
+++ server.js
@@ -18,24 +18,25 @@ try {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.set('port', (process.env.PORT || 3100));
+app.set('port', (process.env.PORT || 3001));
 
 // Express only serves static assets in production
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
+app.use(express.static('client/build'));
 
-app.get('/api/food', (req, res) => {
-  const param = req.query.q;
+// app.get('/api/food', (req, res) => {
+//   const param = req.query.q;
 
-  if (!param) {
-    res.json({
-      error: 'Missing required parameter `q`',
-    });
-    return;
-  }
-})
+//   if (!param) {
+//     res.json({
+//       error: 'Missing required parameter `q`',
+//     });
+//     return;
+//   }
+// })
 
 app.post('/landing_page', (req, res) => {
   console.log(typeof req.body.email);
