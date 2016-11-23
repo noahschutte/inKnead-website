1112f329a8fa13606c0df9541c9a9ee43e9c450a
Koty Wong <wongkoty@gmail.com>
2016-11-23 11:53:18 -0500

    finishing responsive 710-910

--- client/src/index.js
+++ client/src/index.js
@@ -8,6 +8,9 @@ require('./stylesheets/platform.scss');
 require('./stylesheets/how_to.scss');
 require('./stylesheets/request.scss');
 require('./stylesheets/one_step.scss');
+require('./stylesheets/creators.scss');
+require('./stylesheets/subscribe.scss')
+require('./stylesheets/footer.scss');
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
 
