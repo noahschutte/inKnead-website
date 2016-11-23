ae3d84e2bdc189b0904110b04d47b66003155997
Koty Wong <wongkoty@gmail.com>
2016-11-23 17:43:10 -0500

    adding state

--- client/src/components/Subscribe.js
+++ client/src/components/Subscribe.js
@@ -2,6 +2,22 @@ import React, { Component } from 'react';
 import email from '../../public/images/icon_email@2x.png';
 
 export default class Subscribe extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {test: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleChange(e) {
+		console.log(this);
+		console.log(e.target.value);
+		this.setState({test: e.target.value});
+	}
+	handleSubmit(e) {
+		console.log(this.state.test)
+		e.preventDefault();
+	}
   render() {
     return (
       <div className='subscribe'>
@@ -10,13 +26,13 @@ export default class Subscribe extends Component {
 		    	<img src={email}/>
 		    	<p>Learn more on how to use our product and sign up for our newsletter</p>
 		    	<div className='email-form-container'>
-						<form action='/landing_page' method="POST">
-							<input type='text' name='email' placeholder='Your E-mail' className='email-input'/>
+						<form onSubmit={this.handleSubmit}>
+							<input type='text' name='email' placeholder='Your E-mail' value={this.state.test} className='email-input' onChange={this.handleChange}/>
 							<input type='submit' className='white-button' />
 						</form>
 					</div>
 	    	</div>
       </div>
-      )
+    )
   }
 }
