b2f5cc25c94c993e5d9a358d8b865879ec4b0f5b
Koty Wong <wongkoty@gmail.com>
2016-11-23 21:26:50 -0500

    adding preliminary error handling

--- client/src/components/Subscribe.js
+++ client/src/components/Subscribe.js
@@ -5,7 +5,10 @@ import $ from 'jquery';
 export default class Subscribe extends Component {
 	constructor(props) {
 		super(props);
-		this.state = {email: ''};
+		this.state = {
+			email: '',
+			test: null
+		};
 
 		this.handleChange = this.handleChange.bind(this);
 		this.handleSubmit = this.handleSubmit.bind(this);
@@ -22,14 +25,22 @@ export default class Subscribe extends Component {
 	}
 	mailChimp(email) {
 		console.log(email);
+		var test1 = this;
 		$.ajax({
 			url: '/landing_page',
 			method: "POST",
 			data: {
 				email: email,
+				test: false
 			},
 			success: function(data) {
 				console.log(data);
+				if (data == 'success') {
+					test1.setState({test: true})
+				} else {
+					test1.setState({test: false})
+				}
+				console.log(test1.state)
 			},
 			error: function(xhr, status, err) {
 				console.log(status, err.toString());
@@ -45,9 +56,10 @@ export default class Subscribe extends Component {
 		    	<p>Learn more on how to use our product and sign up for our newsletter</p>
 		    	<div className='email-form-container'>
 						<form onSubmit={this.handleSubmit}>
-							<input type='text' name='email' placeholder='Your E-mail' value={this.state.test} className='email-input' onChange={this.handleChange}/>
+							<input type='text' name='email' placeholder='Your E-mail' className='email-input' onChange={this.handleChange}/>
 							<input type='submit' className='white-button' />
 						</form>
+						{"test" + (this.state.test ? "true" : "false")}
 					</div>
 	    	</div>
       </div>
